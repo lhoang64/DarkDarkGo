85b376c7dd1d6e34d9fc9bc42c5aca0bfe540389
Matt <matthewjones@bennington.edu>
2017-11-28 11:12:35 -0500

    Function stubs for interaction with queue and device manager.

---
+++ crawler/mgmt/device_manager.py
@@ -0,0 +1,53 @@
+"""
+    Wrapper for the managment teams device manager endpoints.
+    Nov, 27, 2017 - Matt Jones matthewjones@bennington.edu
+
+    NOTE: For now, this doesn't actually interact with the DM server,
+        it's just a dummy until the server is up.
+"""
+
+class DeviceManager:
+
+    def __init__(self, dm_address):
+        self._address = dm_address.ip
+        self._port = dm_address.port
+
+    def alert_online(self):
+        """
+        The Device manager needs to know that the crawler has started.
+        This method sends an alert to the DM, sayign that we are online.
+
+        TODO: Get request format from mgmt team.
+        """
+        pass
+
+    def send_waiting(self):
+        """
+        In the event that there are no new seeds, we need to tell the DM that
+            we are waiting.
+
+        TODO: Get request format from mgmt team.
+        """
+        pass
+
+    def get_chunks(self):
+        """
+        Requests the list of chunks which should be stored on the crawler.
+
+        Response JSON should look like:
+        [string]
+        """
+        chunks = [1, 2, 3]
+
+        return chunks
+
+    def alert_chunk(chunk_id, address):
+        """
+        Alerts mgmt that we are done this the chunk corresponding to chunk_id.
+
+        param: chunk_id : String corresponding to the id of the chunk we just
+            finished.
+        param: address  : Named Tuple containing our ip and the port to
+            contact us on.
+        """
+        pass
---
+++ crawler/mgmt/queue.py
@@ -0,0 +1,43 @@
+"""
+    Wrapper for the managment teams queue endpoints.
+    Nov, 27, 2017 - Matt Jones matthewjones@bennington.edu
+
+    NOTE: For now, this doesn't actually interact with the queue server,
+        it's just a dummy until the server is up.
+"""
+
+class QueueWrapper:
+
+    def __init__(self, queue_address):
+        self._address = queue_address.ip
+        self._port = queue_address.port
+
+    def get_links(self):
+        """
+        The `/get_links` endpoint returns n links, as well as a new chunk id.
+        Returns a tuple of (list of links, chunk id)
+
+        Response JSON should look like:
+        {
+            "links" : [string],
+            "chunk_id" : string
+        }
+        """
+
+        links = ['https://zqktlwi4fecvo6ri.onion']
+        c_id = 0
+
+        return (links, c_id)
+
+    def add_links(self, links):
+        """
+        The `/add_links` endpoint adds links to the queue (if they are not
+            already handled).
+        prarm: links : A list of strings, where each sring is a .onion url.
+
+        Request JSON should look like:
+        {
+            "links" : [string]
+        }
+        """
+        pass
