8d2ec866565dd42723126206e517ad8b04bc0de8
sashankaryal <sashankaryal@bennington.edu>
2017-11-28 15:05:04 -0500

    Remove torch from 404page.jsx

--- frontend/src/404Page/404Page.jsx
+++ frontend/src/404Page/404Page.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Torch from '../torch.png';
 
 export default () => {
     return (<div id="404">
-        <Torch />
         <h1> 404? :( </h1>
     </div>
     );
