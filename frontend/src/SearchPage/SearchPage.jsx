36b8dbba2fddb8465344ccb66e4f6102a5dc35de
sashankaryal <sashankaryal@bennington.edu>
2017-12-01 10:19:46 -0500

    Search page fetches search results asyncally from the backend server

--- frontend/src/SearchPage/SearchPage.jsx
+++ frontend/src/SearchPage/SearchPage.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import Loader from './Components/PacManLoader';
 
 import './SearchPage.css';
+
+const API_SERVER = "http://localhost:8010/";
 
 export default class SearchPage extends React.Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            query: ""
+            query: "",
+            searchResults: [],
+            searching: true,
+            error: false,
+            overlayMsg: ""
         }
     }
 
@@ -17,30 +24,44 @@ export default class SearchPage extends React.Component {
             const query = url.split("search?q=")[1];
             this.setState(() => {
                 return {query: query}
-            });
+            }, this.search(query));
         } else {
             this.setState(() => {
                 return {query: this.props.location.state.query}
-            });
+            }, this.search(this.props.location.state.query));
         }  
-        this.search();
     }
     
-    search = () => {
-        const query = this.state.query;
-        fetch('http://0.0.0.0:8010/' + query)
-        .then((response) => response.json)
+    search = (query) => {
+        fetch(API_SERVER + query)
+        .catch(()=>{
+            this.setState(()=>{return {error: true, overlayMsg: "Sorry, we couldn't contact the backend server. Make sure your computer is connected to the Internet and try again."}});
+        })
+        .then((response) => response.json())
+        .catch((e) => {this.parseResults("Result is not json")})
         .then((responsejson) => this.parseResults(responsejson))
-        .catch((error) => console.error(error))
+        .catch((error) => console.error(error));
     }
 
-    parseResults = () => {
-        
+    parseResults = (responsejson) => {
+        if (responsejson.head === 'success') {
+            this.setState(()=>{return {searching:false}});
+        }
     }
     
     render() {
-        return (<div className="whitetext" >
-            {this.state.query}</div>
+        if (this.state.searching && !this.state.error) {
+            return <Loader className="loader" loading={true} size={16} margin={-8} />;
+        }
+
+        if (this.state.error) {
+            return <div className="whitetext">{this.state.overlayMsg}</div>;
+        }
+
+        return (
+        <div className="whitetext" >
+
+        </div>
         );
     }
 }
--- frontend/src/server/api/controller.js
+++ frontend/src/server/api/controller.js
@@ -1,4 +1,22 @@
-exports.handleQuery = (req, res) => {
+const Aggregator = require('../Aggregation/Aggregator');
+const Cacher = require('../Caching/Cacher');
+let Manager = require('../Management/Manager');
 
-    res.send("Hi");
+exports.handleQuery = (req, res) => {
+    /*
+        1. Send query 
+        2. Have static aggregation class, and caching class...perhaps they could live on different servers, or not
+
+    */
+    
+    console.log("Manger's IS list is " + Manager.indexServers.empty)
+    if (Manager.indexServers.empty == false) {
+        res.json({
+            head: 'success'
+        });
+    } else {
+        res.json({
+            head: 'error'
+        });
+    }
 }
