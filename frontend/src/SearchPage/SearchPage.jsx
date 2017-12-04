9913fe885553011eb18c2596f101f27c29b73a1d
sashankaryal <sashankaryal@bennington.edu>
2017-12-04 14:27:56 -0500

    Add snippet parser and error handling

--- frontend/src/SearchPage/SearchPage.jsx
+++ frontend/src/SearchPage/SearchPage.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Loader from './Components/PacManLoader';
+import React from 'react'
+import Loader from './Components/PacManLoader'
+import {Link} from 'react-router-dom'
+import './SearchPage.css'
 
-import './SearchPage.css';
-
-const API_SERVER = "http://localhost:8010/";
+const API_SERVER_QUERY_ENDPOINT = "http://localhost:8010/search?q=";
 
 export default class SearchPage extends React.Component {
     
@@ -13,15 +13,14 @@ export default class SearchPage extends React.Component {
             query: "",
             searchResults: [],
             searching: true,
-            error: false,
-            overlayMsg: ""
+            error: ""
         }
     }
 
     componentDidMount() {
         if (this.props.location.state === undefined){
             const url = window.location.href;
-            const query = url.split("search?q=")[1];
+            const query = url.split("search?q=")[1].replace(/[|&;$@"<>()+,]/g, " ").replace(/%20/g,' ');
             this.setState(() => {
                 return {query: query}
             }, this.search(query));
@@ -33,35 +32,74 @@ export default class SearchPage extends React.Component {
     }
     
     search = (query) => {
-        fetch(API_SERVER + query)
+        fetch(API_SERVER_QUERY_ENDPOINT + query)
         .catch(()=>{
-            this.setState(()=>{return {error: true, overlayMsg: "Sorry, we couldn't contact the backend server. Make sure your computer is connected to the Internet and try again."}});
+            this.setState({error: "Sorry, we couldn't contact the backend server. Either your computer is not connected to the Internet or our backend server is offline."})
         })
         .then((response) => response.json())
-        .catch((e) => {this.parseResults("Result is not json")})
-        .then((responsejson) => this.parseResults(responsejson))
+        .catch(() => {if (this.state.error.length === 0) this.setState({error: "Sorry, our backend server has gone berserk. Instead of returning json, they did something else. Email them at sashankaryal@bennington.edu."})})
+        .then((responsejson) => this.parseResult(responsejson))
         .catch((error) => console.error(error));
     }
 
-    parseResults = (responsejson) => {
+    parseResult = (responsejson) => {
         if (responsejson.head === 'success') {
-            this.setState(()=>{return {searching:false}});
+            this.setState((prevState, prevProps) => {
+                return {
+                    searchResults: responsejson.message,
+                    searching: false
+                }
+            })
+        } else {
+            this.setState({searching:false})
+            this.setState({error: responsejson.message})
         }
     }
     
+    changeQuery = (val) => {
+        this.setState(
+            () => {
+              return {query: val}
+            }, () => {
+              //Prefetch and display suggestions
+            }
+          );
+    }
+
+    handleSearch = () => {
+        this.search(this.state.query)
+    }
+
     render() {
-        if (this.state.searching && !this.state.error) {
-            return <Loader className="loader" loading={true} size={16} margin={-8} />;
+        const searchResultsHtml = this.state.searchResults.map((result, index) => {
+            return (
+                <div key={result.href} className={"resultblock_" + index + " results"}>
+                    <div className="resulttitle">
+                        <a className="searchlink" href={result.href}>
+                            {result.title}
+                        </a>
+                    </div>
+                    <div className="resultdesc">
+                        {result.desc}
+                    </div>
+                </div>
+            );
+        })
+
+        if (this.state.error.length > 0)
+            return <div className="whitetext center centertext">{this.state.error}</div>;
+        
+        if (!this.state.searching) {
+            return <div id="searchpage">
+                      <div className="search-bar">
+                            <input id="search-box" onKeyPress={(e)=>{if (e.key === 'Enter') this.handleSearch()}} onChange={(e)=>{this.changeQuery(e.target.value)}} placeholder="Search the dark net" value={this.state.query}/>
+                    </div>
+                <div className="searchresults">
+                    {searchResultsHtml}
+                </div>
+            </div>
         }
 
-        if (this.state.error) {
-            return <div className="whitetext">{this.state.overlayMsg}</div>;
-        }
-
-        return (
-        <div className="whitetext" >
-
-        </div>
-        );
+        return <Loader className="loader center" loading={true} size={16} margin={-8} />
     }
 }
