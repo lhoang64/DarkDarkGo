2ffbbc4998e8cd710f496e701e7181f88df47aee
Hoanh An <hoanhan101@gmail.com>
2017-12-05 02:37:09 -0500

    updated debugging output

--- mgmt/test/database_manager_test.py
+++ mgmt/test/database_manager_test.py
@@ -20,32 +20,32 @@ def test_link_operation():
     # INSERT
     db_manager.operate_on_link_relation('INSERT', link='https://www.example_101.com')
     if db_manager.get_relation('link')[-1]['link'] == 'https://www.example_101.com' and db_manager.get_relation('link')[-1]['state'] == 'pending':
-        print('> Link relation: Test passed for INSERT ')
+        print('> PASSED | INSERT | Link relation')
     else:
-        print('> Link relation: Test failed for INSERT ')
+        print('> FAILED | INSERT | Link relation')
 
     # UPDATE STATE
     db_manager.operate_on_link_relation('UPDATE_STATE', link='https://www.example_101.com', state='crawling')
     if db_manager.get_relation('link')[-1]['state'] == 'crawling':
-        print('> Link relation: Test passed for UPDATE_STATE ')
+        print('> PASSED | UPDATE_STATE | Link relation')
     else:
-        print('> Link relation: Test failed for UPDATE_STATE ')
+        print('> FAILED | UPDATE_STATE | Link relation')
 
     # UPDATE CHUNK ID
     db_manager.operate_on_chunk_relation('INSERT', chunk_id='101c')
     db_manager.operate_on_link_relation('UPDATE_CHUNK_ID', link='https://www.example_101.com', chunk_id='101c')
     if db_manager.get_relation('link')[-1]['chunk_id'] == '101c':
-        print('> Link relation: Test passed for UPDATE_CHUNK_ID ')
+        print('> PASSED | UPDATE_CHUNK_ID | Link relation')
     else:
-        print('> Link relation: Test failed for UPDATE_CHUNK_ID ')
+        print('> FAILED | UPDATE_CHUNK_ID | Link relation')
     db_manager.operate_on_chunk_relation('DELETE', chunk_id='101c')
 
     # DELETE
     db_manager.operate_on_link_relation('DELETE', link='https://www.example_101.com')
     if db_manager.get_length('link') == start_length:
-        print('> Link relation: Test passed for DELETE ')
+        print('> PASSED | DELETE | Link relation')
     else:
-        print('> Link relation: Test failed for DELETE ')
+        print('> FAILED | DELETE | Link relation')
 
 def test_chunk_operation():
     """
@@ -58,16 +58,16 @@ def test_chunk_operation():
     # INSERT
     db_manager.operate_on_chunk_relation('INSERT', chunk_id='101c')
     if db_manager.get_relation('chunk')[-1]['id'] == '101c':
-        print('> Chunk relation: Test passed for INSERT')
+        print('> PASSED | INSERT | Chunk relation')
     else:
-        print('> Chunk relation: Test failed for INSERT')
+        print('> FAILED | INSERT | Chunk relation')
 
     # DELETE
     db_manager.operate_on_chunk_relation('DELETE', chunk_id='101c')
     if db_manager.get_length('chunk') == start_length:
-        print('> Chunk relation: Test passed for DELETE')
+        print('> PASSED | DELETE | Chunk relation')
     else:
-        print('> Chunk relation: Test failed for DELETE')
+        print('> FAILED | DELETE | Chunk relation')
 
 def test_host_operation():
     """
@@ -80,30 +80,30 @@ def test_host_operation():
     # INSERT
     db_manager.operate_on_host_relation('INSERT', host='101.101.101.101:101', type='Crawler')
     if db_manager.get_relation('host')[-1]['host'] == '101.101.101.101:101' and db_manager.get_relation('host')[-1]['state'] == 'offline':
-        print('> Host relation: Test passed for INSERT')
+        print('> PASSED | INSERT | Host relation')
     else:
-        print('> Host relation: Test failed for INSERT')
+        print('> FAILED | INSERT | Host relation')
 
     # UPDATE STATE
     db_manager.operate_on_host_relation('UPDATE_STATE', host='101.101.101.101:101', state='online')
     if db_manager.get_relation('host')[-1]['state'] == 'online':
-        print('> Host relation: Test passed for UPDATE_STATE')
+        print('> PASSED | UPDATE_STATE | Host relation')
     else:
-        print('> Host relation: Test failed for UPDATE_STATE')
+        print('> FAILED | UPDATE_STATE | Host relation')
 
     # UPDATE HEALTH
     db_manager.operate_on_host_relation('UPDATE_HEALTH', host='101.101.101.101:101', health='healthy')
     if db_manager.get_relation('host')[-1]['health'] == 'healthy':
-        print('> Host relation: Test passed for UPDATE_HEALTH')
+        print('> PASSED | UPDATE_HEALTH | Host relation')
     else:
-        print('> Host relation: Test failed for UPDATE_HEALTH')
+        print('> FAILED | UPDATE_HEALTH | Host relation')
 
     # DELETE
     db_manager.operate_on_host_relation('DELETE', host='101.101.101.101:101')
     if db_manager.get_length('host') == start_length:
-        print('> Host relation: Test passed for DELETE')
+        print('> PASSED | DELETE | Host relation')
     else:
-        print('> Host relation: Test failed for DELETE')
+        print('> FAILED | DELETE | Host relation')
 
 def test_crawler_operation():
     """
@@ -121,30 +121,30 @@ def test_crawler_operation():
     # INSERT
     db_manager.operate_on_crawler_relation('INSERT', chunk_id='101c', host='101.101.101.101:101')
     if db_manager.get_relation('crawler')[-1]['chunk_id'] == '101c' and db_manager.get_relation('crawler')[-1]['c_task'] == 'crawling':
-        print('> Crawler relation: Test passed for INSERT')
+        print('> PASSED | INSERT | Crawler relation')
     else:
-        print('> Crawler relation: Test failed for INSERT')
+        print('> FAILED | INSERT | Crawler relation')
 
     # UPDATE HOST
     db_manager.operate_on_crawler_relation('UPDATE_HOST', chunk_id='101c', host='101.101.101.101:102')
     if db_manager.get_relation('crawler')[-1]['c_host'] == '101.101.101.101:102':
-        print('> Crawler relation: Test passed for UPDATE_HOST')
+        print('> PASSED | UPDATE_HOST | Crawler relation')
     else:
-        print('> Crawler relation: Test failed for UPDATE_HOST')
+        print('> FAILED | UPDATE_HOST | Crawler relation')
 
     # UPDATE TASK
     db_manager.operate_on_crawler_relation('UPDATE_TASK', chunk_id='101c', task='crawled')
     if db_manager.get_relation('crawler')[-1]['c_task'] == 'crawled':
-        print('> Crawler relation: Test passed for UPDATE_TASK')
+        print('> PASSED | UPDATE_TASK | Crawler relation')
     else:
-        print('> Crawler relation: Test failed for UPDATE_TASK')
+        print('> FAILED | UPDATE_TASK | Crawler relation')
 
     # DELETE
     db_manager.operate_on_crawler_relation('DELETE', chunk_id='101c')
     if db_manager.get_length('crawler') == start_length:
-        print('> Crawler relation: Test passed for DELETE')
+        print('> PASSED | DELETE | Crawler relation')
     else:
-        print('> Crawler relation: Test failed for DELETE')
+        print('> FAILED | DELETE | Crawler relation')
 
     # Delete temp chunk and host when finish testing
     db_manager.operate_on_chunk_relation('DELETE', chunk_id='101c')
@@ -167,30 +167,30 @@ def test_index_builder_operation():
     # INSERT
     db_manager.operate_on_index_builder_relation('INSERT', chunk_id='101c', host='101.101.101.101:101')
     if db_manager.get_relation('index_builder')[-1]['chunk_id'] == '101c' and db_manager.get_relation('index_builder')[-1]['ib_task'] == 'building':
-        print('> Index Builder relation: Test passed for INSERT')
+        print('> PASSED | INSERT | Index Builder relation')
     else:
-        print('> Index Builder relation: Test failed for INSERT')
+        print('> FAILED | INSERT | Index Builder relation')
 
     # UPDATE HOST
     db_manager.operate_on_index_builder_relation('UPDATE_HOST', chunk_id='101c', host='101.101.101.101:102')
     if db_manager.get_relation('index_builder')[-1]['ib_host'] == '101.101.101.101:102':
-        print('> Index Builder relation: Test passed for UPDATE_HOST')
+        print('> PASSED | UPDATE_HOST | Index Builder relation')
     else:
-        print('> Index Builder relation: Test failed for UPDATE_HOST')
+        print('> FAILED | UPDATE_HOST | Index Builder relation')
 
     # UPDATE TASK
     db_manager.operate_on_index_builder_relation('UPDATE_TASK', chunk_id='101c', task='built')
     if db_manager.get_relation('index_builder')[-1]['ib_task'] == 'built':
-        print('> Index Builder relation: Test passed for UPDATE_TASK')
+        print('> PASSED | UPDATE_TASK | Index Builder relation')
     else:
-        print('> Index Builder relation: Test failed for UPDATE_TASK')
+        print('> FAILED | UPDATE_TASK | Index Builder relation')
 
     # DELETE
     db_manager.operate_on_index_builder_relation('DELETE', chunk_id='101c')
     if db_manager.get_length('index_builder') == start_length:
-        print('> Index Builder relation: Test passed for DELETE')
+        print('> PASSED | DELETE | Index Builder relation')
     else:
-        print('> Index Builder relation: Test failed for DELETE')
+        print('> FAILED | DELETE | Index Builder relation')
 
     # Delete temp chunk and host when finish testing
     db_manager.operate_on_chunk_relation('DELETE', chunk_id='101c')
@@ -214,37 +214,37 @@ def test_index_server_operation():
     # INSERT
     db_manager.operate_on_index_server_relation('INSERT', row=101, chunk_id='101c', host='101.101.101.101:101')
     if db_manager.get_relation('index_server')[-1]['chunk_id'] == '101c' and db_manager.get_relation('index_server')[-1]['row'] == 101:
-        print('> Index Server relation: Test passed for INSERT')
+        print('> PASSED | INSERT | Index Server relation')
     else:
-        print('> Index Server relation: Test failed for INSERT')
+        print('> FAILED | INSERT | Index Server relation')
 
     # UPDATE ROW
     db_manager.operate_on_index_server_relation('UPDATE_ROW', row=102, chunk_id='101c', host='101.101.101.101:101')
     if db_manager.get_relation('index_server')[-1]['row'] == 102:
-        print('> Index Server relation: Test passed for UPDATE_ROW')
+        print('> PASSED | UPDATE_ROW | Index Server relation')
     else:
-        print('> Index Server relation: Test failed for UPDATE_ROW')
+        print('> FAILED | UPDATE_ROW | Index Server relation')
 
     # UPDATE CHUNK ID
     db_manager.operate_on_index_server_relation('UPDATE_CHUNK_ID', chunk_id='102c', row=102, host='101.101.101.101:101')
     if db_manager.get_relation('index_server')[-1]['chunk_id'] == '102c':
-        print('> Index Server relation: Test passed for UPDATE_CHUNK_ID')
+        print('> PASSED | UPDATE_CHUNK_ID | Index Server relation')
     else:
-        print('> Index Server relation: Test failed for UPDATE_CHUNK_ID')
+        print('> FAILED | UPDATE_CHUNK_ID | Index Server relation')
 
     # UPDATE HOST
     db_manager.operate_on_index_server_relation('UPDATE_HOST', host='101.101.101.101:102', row=102, chunk_id='102c')
     if db_manager.get_relation('index_server')[-1]['is_host'] == '101.101.101.101:102':
-        print('> Index Server relation: Test passed for UPDATE_HOST')
+        print('> PASSED | UPDATE_HOST | Index Server relation')
     else:
-        print('> Index Server relation: Test failed for UPDATE_HOST')
+        print('> FAILED | UPDATE_HOST | Index Server relation')
 
     # DELETE
     db_manager.operate_on_index_server_relation('DELETE', row=102, chunk_id='102c', host='101.101.101.101:102')
     if db_manager.get_length('index_server') == start_length:
-        print('> Index Server relation: Test passed for DELETE')
+        print('> PASSED | DELETE | Index Server relation')
     else:
-        print('> Index Server relation: Test failed for DELETE')
+        print('> FAILED | DELETE | Index Server relation')
 
     # Delete temp chunk and host when finish testing
     db_manager.operate_on_chunk_relation('DELETE', chunk_id='101c')
@@ -258,7 +258,3 @@ if __name__ == '__main__':
     test_host_operation()
     test_crawler_operation()
     test_index_builder_operation()
-
-    db_manager = DatabaseManager()
-    results = db_manager.get_all_relations_for_all_chunks()
-    pprint(results)
