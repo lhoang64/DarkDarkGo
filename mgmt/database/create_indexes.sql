8b5added392cc6c4744a64ab32f18658e5879a97
Hoanh An <hoanhan101@gmail.com>
2017-12-05 14:59:58 -0500

    updated creating indexes, changed to non unique ones

--- mgmt/database/create_indexes.sql
+++ mgmt/database/create_indexes.sql
@@ -4,13 +4,13 @@
 -- Date: 12/4/17
 -- -----------------------------------------------------
 
-CREATE UNIQUE INDEX link_chunk_id_idx on link (chunk_id);
-CREATE UNIQUE INDEX chunk_id_idx on chunk (id);
-CREATE UNIQUE INDEX host_idx on host (host);
-CREATE UNIQUE INDEX crawler_chunk_id_idx on crawler (chunk_id);
-CREATE UNIQUE INDEX crawler_c_host_idx on crawler (c_host);
-CREATE UNIQUE INDEX index_builder_chunk_id_idx on index_builder (chunk_id);
-CREATE UNIQUE INDEX index_builder_ib_host_idx on index_builder (ib_host);
-CREATE UNIQUE INDEX index_server_row_idx on index_server (row);
-CREATE UNIQUE INDEX index_server_chunk_id_idx on index_server (chunk_id);
-CREATE UNIQUE INDEX index_server_is_host_idx on index_server (is_host);
+CREATE INDEX link_chunk_id_idx on link (chunk_id);
+CREATE INDEX chunk_id_idx on chunk (id);
+CREATE INDEX host_idx on host (host);
+CREATE INDEX crawler_chunk_id_idx on crawler (chunk_id);
+CREATE INDEX crawler_c_host_idx on crawler (c_host);
+CREATE INDEX index_builder_chunk_id_idx on index_builder (chunk_id);
+CREATE INDEX index_builder_ib_host_idx on index_builder (ib_host);
+CREATE INDEX index_server_row_idx on index_server (row);
+CREATE INDEX index_server_chunk_id_idx on index_server (chunk_id);
+CREATE INDEX index_server_is_host_idx on index_server (is_host);
