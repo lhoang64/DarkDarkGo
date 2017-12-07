f2a22cc28c32a9fd9e5c673d69048c1c9d5b98e2
sashankaryal <sashankaryal@bennington.edu>
2017-12-06 19:46:52 -0500

    Update 404 page

--- frontend/src/404Page/404Page.jsx
+++ frontend/src/404Page/404Page.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './404Page.css';
 
 export default () => {
-    return (<div id="404">
-        <h1> 404? :( </h1>
-    </div>
+    return (
+        <div className="whitetext f04page">
+            <h1>Sorry I don't have a proper 404 page. :(</h1>
+        </div>
     );
 }
