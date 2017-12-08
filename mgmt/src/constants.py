abdc42e3c7c64151077182a4dda787844f02902c
Nidesh Chitrakar <nideshchitrakar@bennington.edu>
2017-12-08 00:29:13 -0500

    added a file to manage constants

---
+++ mgmt/src/constants.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+"""
+    constants.py - Contains all constants used by the device manager
+    Author:
+        - Nidesh Chitrakar (nideshchitrakar@bennington.edu)
+        - Hoanh An (hoanhan@bennington.edu)
+    Date: 12/07/2017
+"""
+
+number_of_rows = 3              # total number rows of Index Servers
+number_of_links = 5             # number of links to be sent to Crawler
+number_of_chunks = 5            # number of chunks to be sent to Index Builder
