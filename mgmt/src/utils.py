6d7882287de126efda8e0318e4c0b79c9da222d6
Nidesh Chitrakar <nideshchitrakar@bennington.edu>
2017-12-08 00:31:27 -0500

    comments, fixes, minor edits

--- mgmt/src/utils.py
+++ mgmt/src/utils.py
@@ -7,13 +7,10 @@
     Date: 11/28/2017
 """
 
-from mgmt.src.database_manager import DatabaseManager
-from pprint import pprint
+from mgmt.src.database_manager import DatabaseManager as db_manager
+from mgmt.src.constants import number_of_rows
 
 id_counter = 0
-number_of_rows = 3          # total number rows of Index Servers
-
-db_manager = DatabaseManager()
 
 def generate_chunk_id():
     """
@@ -26,7 +23,7 @@ def generate_chunk_id():
 
 def distribute_index_servers():
     """
-    Distribute a list of servers to a given number of rows.
+    Distribute a list of index servers to a set number of rows
     :return: List
     """
     index_servers = db_manager.get_all_index_servers()
@@ -35,7 +32,7 @@ def distribute_index_servers():
     row_count = 1
     rows = []
 
-    k = 0  # index in index_servers array
+    k = 0   # the index in index_servers array
     while number_of_index_servers > 0:
         for i in range(0, number_of_rows):
             servers_in_row = []
@@ -51,12 +48,12 @@ def distribute_index_servers():
             number_of_index_servers -= 1
     return rows
 
-# rows = distribute_index_servers()
 
 def assign_index_chunk(rows, chunk_id):
     """
-    Assign chunk id to Index Servers that has been distributed into rows.
-    :param chunk_id: Chunk ID
+    Assign chunk id to Index Servers that has been distributed into rows
+    :param rows: Array of index servers representing rows
+    :param chunk_id: Chunk ID to assign
     :return: None
     """
     for row in rows:
@@ -70,15 +67,4 @@ def assign_index_chunk(rows, chunk_id):
         db_manager.operate_on_index_server_relation('INSERT',row=row['row_num'], chunk_id=chunk_id, host=row['row'][current_index])
         print(row['row'][current_index] + '\n')
 
-
-if __name__ == '__main__':
-    # rows_of_index_servers = distribute_index_servers()
-    #
-    # chunk_relation = db_manager.get_relation('chunk')
-    # for chunk in chunk_relation:
-    #     chunk_id = chunk['id']
-    #     assign_index_chunk(rows_of_index_servers, chunk_id)
-
-    index_server_relation = db_manager.get_relation('index_server')
-    pprint(index_server_relation)
 
