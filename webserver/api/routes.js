8bedf52101e2589e9f5f25ad82fba4492810c72d
sashankaryal <sashankaryal@bennington.edu>
2017-12-09 18:23:28 -0500

    Add /searchcache route

--- webserver/api/routes.js
+++ webserver/api/routes.js
@@ -1,6 +1,7 @@
 module.exports =  (app) => {
-    var controller = require('./controller');
-    app.route('/search').get(controller.handleQuery);
-    app.route('/get_health').get()
-    app.route('*').get(controller.fofhandler);    
+    var controller = require('./controller')
+    app.route('/search').get(controller.handleQuery)
+    app.route('/get_health').get(controller.handleHealth)
+    app.route('/searchcache').get(controller.handleCacheSearch)
+    app.route('*').get(controller.fofhandler)
 }
