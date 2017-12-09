8d91d58ac73f935af08351bec84ced46dfc91de9
sashankaryal <sashankaryal@bennington.edu>
2017-12-09 18:27:22 -0500

    Accept callback for search instead of returning response from the index server

--- webserver/Aggregation/Aggregator.js
+++ webserver/Aggregation/Aggregator.js
@@ -1,23 +1,33 @@
-const Cacher = require('../Caching/Cacher');
+const Cacher = require('../Caching/Cacher')
 const Manager = require('../Management/Manager')
+
+let fetch = require("isomorphic-fetch")
+
+const INDEX_SERVER_DOCID_ENDPOINT = ""
+const INDEX_SERVER_SNIPPET_ENDPOINT = ""
 
 module.exports = Aggregator = 
 {
-    queryIndexServerForDocIds : (query) => {
-        // Check if the query is in the cache, if yes, we can directly call queryIndexServerForSnippets
-        if (Cacher.cache.has(query)) {
-            snippetwithids = cacher.cache.get(query)
-            console.log(String(Date.now()),"Query",query,"served from cache")
-            return queryIndexServerForSnippets(Cacher.cacher.get(query))
-        }
-        const numRows = Manager.indexServers.map.length
-        const randRowIndex = Math.floor((Math.random() * numRows))
-        for (let k in Manager.indexServers.map[randRow].servers) {
-                console.log("hi")
-        }
+    getResultsFromIndexServer : (query, callback) => {
+        query = query.replace(/[|&;$@"<>()+,]/g, " ").replace(/%20/g,' ')
+        Aggregator.queryIndexServerForDocIds(query, callback)
     },
 
-    queryIndexServerForSnippets: (query, docids) => {
+    queryIndexServerForDocIds : (query, callback) => {
+        // Check if the query is in the cache, if yes, we can directly call queryIndexServerForSnippets
+        if (Cacher.cache.has(query)) {
+            snippetwithids = Cacher.cache.get(query)
+            console.log(String(Date.now()),"Query",query,"served from cache")
+            Aggregator.queryIndexServerForSnippets(query, Cacher.cacher.get(query), callback)
+        }
+        responded = false
+        fetch(INDEX_SERVER_DOCID_ENDPOINT + query)
+        .catch((e) => {callback(undefined, "Index server docid endpoint doesn't work"); responded = true})
+        .then((response) => {if (!responded) return response.json()})
+        .then((responsejson) => {if (!responded) Aggregator.queryIndexServerForSnippets(query, responsejson, callback)})
+    },
+
+    queryIndexServerForSnippets: (query, docids, callback) => {
         if (!Cacher.cache.has(query)){
             let snippetwithids = {
                 'query': query,
@@ -26,17 +36,22 @@ module.exports = Aggregator =
             Cacher.addToCache(query, snippetwithids)
         }
 
+        //Only query offset - offset + 10 queries
+
         let error = false
         let errormsg = ""
         let returnvalue = 
-        fetch('INDEXSERVERSNIPPETENDPOINT')
-        .catch((err) => {
+        fetch(INDEX_SERVER_SNIPPET_ENDPOINT, {
+            method: 'POST',
+            body: docids
+        })
+        .catch(() => {
             error = true,
             console.log("Index server's snippet endpoint is offline.")
             errormsg = "Index server's snippet endpoint is offline."
         })
         .then((response) => response.json())
-        .catch((err) => {
+        .catch(() => {
             if (!error){
                 error = true,
                 console.log("Index server did not return a valid JSON.")
@@ -44,15 +59,7 @@ module.exports = Aggregator =
                 throw err
             }
         })
-        .then((responsejson) => responsejson
-        )
-    },
-
-    reportunsuccessfuloperation: (errormsg) => {
-        return {
-            success: false,
-            body: errormsg
-        }
+        .then((responsejson) => responsejson)
     }
 }
 
