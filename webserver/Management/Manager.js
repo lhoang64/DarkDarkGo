9253dfe6056bd755865dfbb36bd33f67d9044fd5
sashankaryal <sashankaryal@bennington.edu>
2017-12-09 19:24:49 -0500

    Add buildInverseMap

--- webserver/Management/Manager.js
+++ webserver/Management/Manager.js
@@ -9,44 +9,59 @@ let fetch = require("isomorphic-fetch")
 const Cacher = require('../Caching/Cacher')
 const Aggregator = require('../Aggregation/Aggregator')
 
-const MGMT_IP = ""
+const MGMT_ENDPOINT = "http://0.0.0.0/get_map/index_servers"
 
 // Manager is a singleton object, accessed from both server.js and controller.js in API
-module.exports = Manager = 
-{
-    indexServers : {
+module.exports = Manager = {
+    start: () => {
+        Cacher.initializeCache()
+        // Repopulate it every 1 minute
+        //Manager.intervalid = setInterval(Manager.populateIndexServers(), 1000 * 60);
+    },
+
+    indexServers: {
         empty: true,
         map: [],
         inversemap: []
     },
 
-    populateIndexServers : () => {
-        fetch(MGMT_IP + "/get_map/index_servers")
-        .catch((e)=>{
-            console.log("Couldn't fetch.");
-            throw e
-        })
-        .then((response) => response.json())
-        .catch((e)=>{
-            throw e
-        })
-        .then((responsejson) => {
-            Manager.indexServers = responsejson
-            Manager.indexServers.empty = false
-        })
-        .catch((error) => {
-            console.log(error.message)
+    populateIndexServers: () => {
+        fetch(MGMT_ENDPOINT)
+            .catch((e) => {
+                console.log(Date.now(), "Management server is offline.");
+                throw e
+            })
+            .then((response) => response.json())
+            .catch((e) => {
+                throw e
+            })
+            .then((responsejson) => {
+                Manager.indexServers.map = responsejson
+                Manager.indexServers.empty = false
+                Manager.buildInverseMap()
+            })
+            .catch((error) => {
+                console.log(Date.now(), error.message)
+            })
+    },
+
+    buildInverseMap: () => {
+        let temp = {}
+        Manager.indexServers.map.forEach((row) => {
+            row.forEach((indexServer) => {
+                indexServer["chunk_ids"].forEach((chunkId) => {
+                    if (temp[chunkId]) {
+                        temp[chunkId].add(indexServer["host"])
+                    } else {
+                        temp[chunkId] = new Set()
+                        temp[chunkId].add(indexServer["host"])
+                    }
+                })
+            })
         })
     },
 
-    start : () => {
-        Cacher.initializeCache()
-
-        // Repopulate it every 3 minutes
-        //Manager.populateIndexServers();
-    },
-
-    stop : () => {
+    stop: () => {
         if (Cacher.intervalid)
             clearInterval(Cacher.intervalid)
         Cacher.dumpCacheToPersistenceSync()
