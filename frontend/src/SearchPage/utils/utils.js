95d5434746016bb3d50a4d0d35a3f31b0a8d05d5
sashankaryal <sashankaryal@bennington.edu>
2017-12-09 22:49:45 -0500

    Parse query and export function

--- frontend/src/SearchPage/utils/utils.js
+++ frontend/src/SearchPage/utils/utils.js
@@ -1,6 +1,8 @@
-getQueryString = ( field, url ) => {
-    var href = url ? url : window.location.href
-    var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' )
-    var string = reg.exec(href)
-    return string ? string[1] : null
+module.exports = { 
+    getQueryString : function(field, url) {
+        var href = url ? url : window.location.href
+        var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' )
+        var string = reg.exec(href)
+        return string ? string[1] : null
+    }
 }
